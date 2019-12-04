f6a48f2e763c47d472214c0022e18c0d460c00bd
unknown <ziad32ahmed111@gmail.com>
2019-12-04 15:41:09 +0200

    it almost ready for backend

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* start navbar */
 
 let saved_btn = $('.navbar .saved button');
-let account_btn = $('.navbar .account');$ 
+let account_btn = $('.navbar .account button');$ 
 
 $('.navbar-light .navbar-toggler').click(() => {
     saved_btn.toggleClass("disabled");
